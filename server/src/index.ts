56fe3cb2d848bc9dbac8397c041d570830ce1df7
michalmilek <mmilek1337@gmail.com>
2023-05-16 16:20:54 +0200

    fix(server): node path added

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,4 +20,14 @@ app.use("/api", bidsRoute);
 app.use("/api", notificationRoute);
 
 
+const path = require("path");
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
