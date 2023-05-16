5050f85db6d33ebbea56b55ab11d648419250ec5
michalmilek <mmilek1337@gmail.com>
2023-05-16 17:34:13 +0200

    feat(cors): CORS added to server

--- client/src/apicalls/axiosInstance.ts
+++ client/src/apicalls/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
 import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
 import morgan from 'morgan'
+import cors from "cors";
 require("dotenv").config();
-const dbConfig = require('./config/dbConfig')
-const usersRoute = require('./routes/usersRoute')
-const productRoute = require('./routes/productRoute')
+const dbConfig = require("./config/dbConfig");
+const usersRoute = require("./routes/usersRoute");
+const productRoute = require("./routes/productRoute");
 const bidsRoute = require("./routes/bidsRoute");
 const notificationRoute = require("./routes/notificationsRoute");
 
 const app = express();
 const port = process.env.PORT || 6000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("common"));
 
@@ -19,14 +21,15 @@ app.use("/api", productRoute);
 app.use("/api", bidsRoute);
 app.use("/api", notificationRoute);
 
-
 const path = require("path");
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 
