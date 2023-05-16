0a4e7f3ed129bdbb3ae4728a8344c094c8f3d498
michalmilek <mmilek1337@gmail.com>
2023-05-16 17:52:57 +0200

    fix(client): testing client deploying

--- client/src/apicalls/axiosInstance.ts
+++ client/src/apicalls/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: "https://server-acfl.onrender.com",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
