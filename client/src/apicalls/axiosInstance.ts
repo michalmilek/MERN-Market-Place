6945aaaa603cdc2be1b22c913e905d71a4f95324
Michał Miłek <112391810+michalmilek@users.noreply.github.com>
2023-05-24 09:58:23 +0200

    fix(axiosInstance): moved api url to environment variable

--- client/src/apicalls/axiosInstance.ts
+++ client/src/apicalls/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://server-acfl.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
